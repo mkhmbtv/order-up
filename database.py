81b3d1f49c30bad04afa45a3a42c6cb8e46da33c
your name here <mmakhambetov@yandex.ru>
2022-01-26 00:48:35 +0400

    Table model and data

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
+from random import randint
+
+from flask import session
 load_dotenv()
 
 from app import app, db
-from app.models import Employee, Menu, MenuItem, MenuItemType
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
 
 
 with app.app_context():
@@ -24,6 +27,9 @@ with app.app_context():
     fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
     drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
     jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+    for i in range(1, 11):
+        db.session.add(Table(number=i, capacity=randint(2, 8)))
 
     db.session.add(dinner)
     db.session.add(employee)
