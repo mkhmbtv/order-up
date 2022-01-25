dcdf12036a5ca214f13fe974cf3ceddec46f4489
your name here <mmakhambetov@yandex.ru>
2022-01-25 20:13:33 +0400

    Create basic Flask app layout

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from .config import Configuration
+from .routes import orders
+
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+app.register_blueprint(orders.bp)
